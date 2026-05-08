1e872dab67cb72b174be2ae95f07dde96a4ae4d3
BYKOfficial <qamin219@gmail.com>
2026-05-08 10:45:59 +0530

    Fix db connection force pooler URL

--- src/db.js
+++ src/db.js
@@ -2,9 +2,10 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL || 'postgresql://postgres.ilhpjxuzafojdtrxjubc:BYK767002amin@aws-1-ap-south-1.pooler.supabase.com:5432/postgres',
   ssl: {
     rejectUnauthorized: false
   }
 });
+
 module.exports = pool;
